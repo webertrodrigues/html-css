bcf74adbfd958247b568b6f542ed8b229f2d4954
Webert Rodrigues Caldeira da silva <129799974+webertrodrigues@users.noreply.github.com>
2024-04-21 00:12:38 -0300

    Initial commit

---
+++ Desafios/d009/link cursos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+    int x, y, z;
+    Printf("Digite valor de x: ");
+    scanf("%i", &x);
+    Printf("Digite valor de y: ");
+    scanf("%i", &y);
+    z = x+y;
+    printf("O valor da soma desses números é de %i", z);
+}
